74ecc6e8b5f026fd4c210587e3b4b2cd203d674c
Gabriel Abreu <gabrielabreu.development@gmail.com>
2024-07-19 13:24:48 -0300

    Just testing

---
+++ tests/Haseroz.DevKit.AspNetCore.UnitTests/MinimalApiEndpointTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Routing;
+using NSubstitute;
+
+namespace Haseroz.DevKit.AspNetCore.UnitTests;
+
+public class MinimalApiEndpointTests
+{
+    [Fact]
+    public void Teste()
+    {
+        var endpoint = new ConcreteMinimalApiEndpoint();
+
+        var builder = Substitute.For<IEndpointRouteBuilder>();
+        endpoint.Define(builder);
+        Assert.True(true);
+    }
+}
+
+
+public class ConcreteMinimalApiEndpoint : MinimalApiEndpoint
+{
+    public override void Define(IEndpointRouteBuilder builder)
+    {
+    }
+}
